a7c1d0eb2e3919a5a066d42afe45db58f94c20ea
Saurav <spike.samantray@gmail.com>
2020-06-24 15:28:55 +0530

    Updates

---
+++ precache-manifest.897c6b7ccddff7957697aa83a6c55e74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57aa32be5e3ef79e26305577bd0ce1ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "297f963a5ff9e5e89b61",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0084a9c0caa1c874933f",
+    "url": "/static/js/2.0b88b9a0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0b88b9a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "297f963a5ff9e5e89b61",
+    "url": "/static/js/main.e5e6e854.chunk.js"
+  },
+  {
+    "revision": "e0a0605584df5b3ec7b9",
+    "url": "/static/js/runtime-main.ebaec484.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
